294dc2ef931b9570aa04bfb7570b20d310be9aea
Daniel Zotti <daniel.zotti87@gmail.com>
2022-01-28 11:33:05 +0100

    Add Direction type + add has next/prev page methods

--- 1-intro-and-basics/src/bonus-point/api/paginator.service.ts
+++ 1-intro-and-basics/src/bonus-point/api/paginator.service.ts
@@ -1,46 +1,48 @@
-import { getCharacters } from "../../exercise/api/rick-and-morty.service.js";
+import { getCharacters } from '../../exercise/api/rick-and-morty.service.js';
 import {
-  CharactersPaginatorOptions,
+  CharactersPaginatorOptions, Direction,
   PaginatedCharactersResponse,
-} from "../models/paginator.models";
+} from '../models/paginator.models';
 
 export async function* getCharactersPaginator(
   { page, direction }: CharactersPaginatorOptions = {
     page: 1,
     direction: 1,
   }
-): AsyncGenerator<
+): AsyncGenerator<PaginatedCharactersResponse,
   PaginatedCharactersResponse,
-  PaginatedCharactersResponse,
-  Partial<CharactersPaginatorOptions>
-> {
-  function changeDirection(newDir: -1 | 1) {
-    direction = newDir;
-  }
-  function changePage(newPage: number) {
-    currentPage = newPage >= 1 ? newPage : 1;
-  }
+  Partial<CharactersPaginatorOptions>> {
 
   let currentPage: number;
+
+  const changeDirection = (newDir: Direction): Direction => direction = newDir;
+
+  const changePage = (newPage: number): number => currentPage = newPage >= 1 ? newPage : 1;
+
+  const hasNextPage = ({ next, direction }: { next: string; direction: Direction }): boolean => !!next && direction > 0;
+
+  const hasPrevPage = ({ prev, direction }: { prev: string; direction: Direction }): boolean => !!prev && direction < 0;
+
   changePage(page);
 
-  while (true) {
+  while(true) {
     const { direction: newDir, page: newPage } =
-      (yield getCharacters(currentPage).then((res) => {
-        const payload = { response: res, page: currentPage };
-        if (res.info) {
-          if (res.info.next && direction > 0) {
-            changePage(currentPage + 1);
-          } else if (res.info.prev && direction < 0) {
-            changePage(currentPage - 1);
-          } else {
-            changeDirection(-direction as -1 | 1);
-            changePage(currentPage + direction);
-          }
+    (yield getCharacters(currentPage).then((res) => {
+      const payload = { response: res, page: currentPage };
+      if(res.info) {
+        const { next, prev } = res?.info;
+        if(hasNextPage({ next, direction })) {
+          changePage(currentPage + 1);
+        } else if(hasPrevPage({ prev, direction })) {
+          changePage(currentPage - 1);
+        } else {
+          changeDirection(-direction as Direction);
+          changePage(currentPage + direction);
         }
-        return payload;
-      })) || {};
-    newDir != null && changeDirection(newDir);
-    newPage != null && changePage(newPage);
+      }
+      return payload;
+    })) || {};
+    !!newDir && changeDirection(newDir);
+    !!newPage && changePage(newPage);
   }
 }
--- 1-intro-and-basics/src/bonus-point/models/paginator.models.ts
+++ 1-intro-and-basics/src/bonus-point/models/paginator.models.ts
@@ -1,11 +1,13 @@
-import { CharactersResponse } from "../../exercise/models/rick-and-morty.models";
+import { CharactersResponse } from '../../exercise/models/rick-and-morty.models';
 
 export interface CharactersPaginatorOptions {
   page: number;
-  direction: 1 | -1;
+  direction: Direction;
 }
 
 export interface PaginatedCharactersResponse {
   response: CharactersResponse;
   page: number;
 }
+
+export type Direction = 1 | -1;
